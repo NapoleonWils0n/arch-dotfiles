422ef918b3f15a6d938f247eba5f490e7cd5635f
Daniel J Wilcox <danieljwilcox@gmail.com>
2016-11-12 23:45:11 +0000

    emacs

--- .emacs.d/elpa/git-auto-commit-mode-20150404.751/git-auto-commit-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "git-auto-commit-mode" "20150404.751" "Emacs Minor mode to automatically commit and push" 'nil :url "http://projects.ryuslash.org/git-auto-commit-mode/" :keywords '("vc"))
--- .emacs.d/elpa/ox-pandoc-20161101.1920/ox-pandoc-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ox-pandoc" "20161101.1920" "org exporter for pandoc." '((org "8.2") (emacs "24") (dash "2.8") (ht "2.0")) :url "https://github.com/kawabata/ox-pandoc" :keywords '("tools"))
